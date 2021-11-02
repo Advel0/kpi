8018b048287f9c4c1d2a67300addf7a7500bac45
Advel0 <61388511+Advel0@users.noreply.github.com>
2021-11-02 17:46:25 +0200

    test of the algorithm(5 lab)

--- op/lab5/lab5.cpp
+++ op/lab5/lab5.cpp
@@ -10,24 +10,36 @@ int main() {
     cin >> p;
     cout << "Enter value of the q variable: ";
     cin >> q;
-    cout << "Divisors of p, which are coprime with q:"<< endl;
+    
     for (int i = 1; i < p+1; i++) {
+  //      cout << "-------------" << endl << "iteration: " << i<<endl;
+  //      cout << "(p % i == 0) = " << (p % i == 0) << endl; 
         if (p % i == 0) {
             pDivisor = i ;
+     //       cout << "pDivisor = " << pDivisor<<endl;
             a = pDivisor;
             b = q;
+  //          cout <<"a = " <<a<<endl<<"b = "<< b<<endl;
             // Euclid algorithm
+      //      cout << "Euclid algorithm"<<endl;
             while (a != 0 && b != 0) {
+                cout << "(a != 0 && b != 0) = " <<(a !=0 && b!=0) << endl;
+        //        cout << "---" <<endl;
                 if (a > b) {
                     a %= b;
+        //            cout << "a<b =  "<< (a<b) << endl;
                 }
                 else {
                     b %= a;
+       //             cout << "a<b = "<< (a<b) << endl;
                 }
+         //       cout <<"a = " << a << endl <<"b = "<< b <<endl;
             }
             biggestCommonDivisor = a+b;
+     //       cout <<"biggestCommonDivisor = "<< biggestCommonDivisor<<endl;
             if ( biggestCommonDivisor == 1){
-                cout <<pDivisor << endl;
+     //           cout << "( biggestCommonDivisor == 1) = " << ( biggestCommonDivisor == 1)<< endl;
+                cout <<"виведення "<<pDivisor << endl;
                 }
              
         }
