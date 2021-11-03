0c3afecc07d0b76eaeb7ee47e8c87c4997fdba3d
Advel0 <61388511+Advel0@users.noreply.github.com>
2021-11-03 12:21:37 +0200

    lab5

--- op/lab5/lab5.cpp
+++ op/lab5/lab5.cpp
@@ -12,36 +12,22 @@ int main() {
     cin >> q;
     
     for (int i = 1; i < p+1; i++) {
-  //      cout << "-------------" << endl << "iteration: " << i<<endl;
-  //      cout << "(p % i == 0) = " << (p % i == 0) << endl; 
         if (p % i == 0) {
             pDivisor = i ;
-     //       cout << "pDivisor = " << pDivisor<<endl;
             a = pDivisor;
             b = q;
-  //          cout <<"a = " <<a<<endl<<"b = "<< b<<endl;
-            // Euclid algorithm
-      //      cout << "Euclid algorithm"<<endl;
             while (a != 0 && b != 0) {
-                cout << "(a != 0 && b != 0) = " <<(a !=0 && b!=0) << endl;
-        //        cout << "---" <<endl;
                 if (a > b) {
                     a %= b;
-        //            cout << "a<b =  "<< (a<b) << endl;
                 }
                 else {
                     b %= a;
-       //             cout << "a<b = "<< (a<b) << endl;
                 }
-         //       cout <<"a = " << a << endl <<"b = "<< b <<endl;
             }
             biggestCommonDivisor = a+b;
-     //       cout <<"biggestCommonDivisor = "<< biggestCommonDivisor<<endl;
             if ( biggestCommonDivisor == 1){
-     //           cout << "( biggestCommonDivisor == 1) = " << ( biggestCommonDivisor == 1)<< endl;
-                cout <<"виведення "<<pDivisor << endl;
+                cout<<pDivisor << endl;
                 }
-             
         }
     }
     return 0;
